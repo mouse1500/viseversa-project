ef5ebe582059ec9f762a5ed3c3a7290c2b64493a
Lavlet <iberezin1500@gmail.com>
2020-08-02 23:31:43 +0300

    Added added counting of entered words

--- viseversa/views.py
+++ viseversa/views.py
@@ -5,5 +5,8 @@ def home(request):
 
 def revers(request):
 	user_text = request.GET['usertext']
+	words = user_text.split()
+	number_of_words = len(words)
 	reversed_text = user_text[::-1]
-	return render(request, 'revers.html',{'usertext':user_text, 'reversedtext':reversed_text})
+
+	return render(request, 'revers.html',{'usertext':user_text, 'reversedtext':reversed_text, 'number_of_words':number_of_words})
